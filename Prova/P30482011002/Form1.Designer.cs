24905d675b51915a6ba985837a0a66d546d6a4f5
Amanda-Shichinoe <amanda.ncosta@hotmail.com>
2020-11-17 10:43:00 -0300

    Avaliacao

---
+++ Prova/P30482011002/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace P30482011002
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnVerificar = new System.Windows.Forms.Button();
+            this.lbxSaida = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnVerificar
+            // 
+            this.btnVerificar.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnVerificar.Location = new System.Drawing.Point(102, 191);
+            this.btnVerificar.Name = "btnVerificar";
+            this.btnVerificar.Size = new System.Drawing.Size(174, 56);
+            this.btnVerificar.TabIndex = 0;
+            this.btnVerificar.Text = "Verificar";
+            this.btnVerificar.UseVisualStyleBackColor = true;
+            this.btnVerificar.Click += new System.EventHandler(this.btnVerificar_Click);
+            // 
+            // lbxSaida
+            // 
+            this.lbxSaida.FormattingEnabled = true;
+            this.lbxSaida.Location = new System.Drawing.Point(408, 27);
+            this.lbxSaida.Name = "lbxSaida";
+            this.lbxSaida.Size = new System.Drawing.Size(361, 394);
+            this.lbxSaida.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lbxSaida);
+            this.Controls.Add(this.btnVerificar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnVerificar;
+        private System.Windows.Forms.ListBox lbxSaida;
+    }
+}
+
---
+++ Prova/P30482011002/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+
+namespace P30482011002
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnVerificar_Click(object sender, EventArgs e)
+        {
+            int quantidadeMeses = 2, semanasMes = 4; //final do meu RA = 2 , logo quantidadeMeses = 2
+            double totalVendidoMes = 0, totalGeral = 0;
+            double[,] vendas = new double[quantidadeMeses, semanasMes];
+            string valor;
+            bool avancar;
+
+            //Validando valores numéricos
+            for (int i = 0; i < quantidadeMeses; i++)
+            {
+                for (int j = 0; j < semanasMes; j++)
+                {
+                    avancar = false;
+                    do
+                    {
+                        valor = Interaction.InputBox("Mês: " + (i + 1) + ", Semana: " + (j + 1));
+                        if (double.TryParse(valor, out vendas[i, j]))
+                        {
+                            totalGeral += vendas[i, j];
+                            avancar = true;
+                        }
+
+                        else
+                            MessageBox.Show("Entrada Inválida!");
+                    } while (avancar == false);
+                }
+            }
+
+            // imprimindo a saída no ListBox
+            lbxSaida.BeginUpdate();
+            for (int i = 0; i < quantidadeMeses; i++)
+            {
+                for (int j = 0; j < semanasMes; j++)
+                {
+                    lbxSaida.Items.Add("Total do mês: " + (i + 1) + " Semana:" + (j + 1) + " R$" + vendas[i, j].ToString("N2"));
+                    totalVendidoMes += vendas[i, j];
+                    totalGeral += vendas[i, j];
+                }
+                lbxSaida.Items.Add(">> Total Mês: R$" + totalVendidoMes.ToString("N2"));
+                lbxSaida.Items.Add("---------------------------");
+            }
+            lbxSaida.Items.Add(">> Total Geral: R$" + totalGeral.ToString("N2"));
+            lbxSaida.EndUpdate();
+        }
+    }
+}
